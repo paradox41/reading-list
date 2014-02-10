aff57904ddb5e61509a66f727b039ec747cb7622
Will <will@coefficientinc.com>
2014-02-09 21:56:23 -0500

    Add REST api

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var express  = require('express');
-var app      = express();
+var express = require('express');
+var app = express();
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost');
@@ -18,6 +18,55 @@ var Book = mongoose.model('Book', {
     dateFinished: Date,
     createdOn: Date,
     updatedOn: Date
+});
+
+// api stuff
+app.get('/api/books', function(request, response) {
+
+    Book.find(function(error, books) {
+        // if there is an error, send the error
+        if (error) {
+            response.send(error);
+        }
+
+        // otherwise just send the books json
+        response.json(books);
+    });
+});
+
+app.post('/api/books', function(request, response) {
+
+    Book.create({
+        title: request.body.title,
+        author: request.body.author,
+        number_of_pages: request.body.numberOfPages,
+        date_started: request.body.dateStarted,
+        date_finished: request.body.dateFinished,
+        created_on: new Date(),
+        updated_on: new Date()
+    }, function(error, book) {
+        if (error) {
+            response.send(error);
+        }
+
+        Book.find(function(error, books) {
+            if (error) {
+                response.send(error);
+            }
+
+            response.json(books);
+        });
+    });
+});
+
+app.delete('/api/books/:book_id', function(request, response) {
+    Book.remove({
+        _id: request.params.book_id
+    }, function(error, book) {
+        if (error) {
+            response.send(error);
+        }
+    });
 });
 
 app.listen(8080);
