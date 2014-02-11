c64d693dd4693f30fd190ae12057061b2eeeee30
Will <will@coefficientinc.com>
2014-02-10 21:08:17 -0500

    More config

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 'use strict';
 
-var express = require('express');
-var app = express();
+var express  = require('express');
+var app      = express();
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost');
 
 app.configure(function() {
+    app.use(express.static(__dirname + '/app'));
+    app.use(express.bodyParser());
     app.use(express.logger('dev'));
 });
 
